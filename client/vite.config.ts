ff0e5e2f602b5c431d317dacd116e35312be132f
yasirw212 <a href=/cdn-cgi/l/email-protection class=__cf_email__ data-cfemail=afd6c0daddf0cac2cec6c3efcad7cec2dfc3ca81ccc0c2[email&nbsp;protected]/a>
2024-09-15 05:57:04 +0000

    vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    strictPort: true,
+    port: 8081
   }
 })
