4bcce73503120ae5262a52c2117fe73539e43d92
yasirw212 <a href=/cdn-cgi/l/email-protection class=__cf_email__ data-cfemail=afd6c0daddf0cac2cec6c3efcad7cec2dfc3ca81ccc0c2[email&nbsp;protected]/a>
2024-09-15 05:53:35 +0000

    Setting up express authentication

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,20 +2,21 @@ import express, {Request, Response} from "express";
 import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose";
+import userRoutes from './routes/users'
+import authRoutes from './routes/auth'
 
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
-app.get('/', (req: Request, res: Response) => {
-    res.json({message: 'gangnem'})
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 mongoose.connect(process.env.MONGODB_URI as string, {
     
 })  
-.then(() => app.listen(4001, () => {
-    console.log(`Listening on localhost:4001...`)
+.then(() => app.listen(7000, '0.0.0.0', () => {
+    console.log(`Listening on localhost:7000...`)
 }))
 .catch(err => console.log(err))
