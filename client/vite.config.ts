72142500aec59aff027239c6afcb691e11493bbb
Yasir Williams <105167851+yasirw212@users.noreply.github.com>
2024-09-18 23:03:02 -0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true,
     strictPort: true,
     port: 80
   }
