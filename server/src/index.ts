68e364367b5a1669147cd17d728113db6c612b6b
yasirw212 <a href=/cdn-cgi/l/email-protection class=__cf_email__ data-cfemail=afd6c0daddf0cac2cec6c3efcad7cec2dfc3ca81ccc0c2[email&nbsp;protected]/a>
2024-09-25 20:06:51 +0000

    Creating Registration Route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from "express";
-import cors from "cors"
-import "dotenv/config"
+import cors from "cors";
+import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from './routes/users'
 import authRoutes from './routes/auth'
@@ -10,13 +10,15 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 mongoose.connect(process.env.MONGODB_URI as string, {
     
 })  
-.then(() => app.listen(7000, '0.0.0.0', () => {
+.then(() => app.listen(7000, () => {
     console.log(`Listening on localhost:7000...`)
 }))
 .catch(err => console.log(err))
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -41,7 +41,7 @@ router.post("/register", [
             secure: process.env.NODE_ENV === "production",
             maxAge: 86400000,
         });
-        return res.sendStatus(200);
+        return res.status(200).json({message: "User registered OK"});
     } catch (error) {
         console.log(error);
         res.status(500).json({message: "Something went wrong"});
