5cb5d7c6dd9f2d32a8d3ae956ca37fa5826c35fb
yasirw212 <williamsyasir90@gmail.com>
2024-10-31 22:53:21 -0500

    new build

--- server/dist/index.js
+++ server/dist/index.js
@@ -9,16 +9,27 @@ require("dotenv/config");
 const mongoose_1 = __importDefault(require("mongoose"));
 const users_1 = __importDefault(require("./routes/users"));
 const auth_1 = __importDefault(require("./routes/auth"));
+const my_hotels_1 = __importDefault(require("./routes/my-hotels"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const path_1 = __importDefault(require("path"));
+const cloudinary_1 = require("cloudinary");
+cloudinary_1.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 const app = (0, express_1.default)();
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use((0, cors_1.default)({
-    origin: [process.env.FRONTEND_URL, "http://34.122.199.84", "http://localhost:8081"]
-}));
+app.use((0, cors_1.default)());
+app.use(express_1.default.static(path_1.default.join(__dirname, "../../client/dist")));
 app.use("/api/auth", auth_1.default);
 app.use("/api/users", users_1.default);
+app.use("/api/my-hotels", my_hotels_1.default);
+app.get("*", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '../../client/dist/index.html'));
+});
 mongoose_1.default.connect(process.env.MONGODB_URI, {})
     .then(() => console.log("Connected to database: ", process.env.MONGODB_URI))
     .then(() => app.listen(4001, () => {
